df119156bd905375666b83ff708fdaa8e62200d0
Miguel-hash85 <msanchezherrero7@gmail.com>
2021-12-20 12:20:46 +0100

    LastSIgnIn class created

---
+++ src/java/entities/LastSignIn.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author 2dam
+ */
+@Entity
+public class LastSignIn implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private Calendar lastSignIn;
+    
+    @ManyToOne
+    private User user;
+
+    public Calendar getLastSignIn() {
+        return lastSignIn;
+    }
+
+    public void setLastSignIn(Calendar lastSignIn) {
+        this.lastSignIn = lastSignIn;
+    }
+    
+    @XmlTransient
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LastSignIn other = (LastSignIn) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "LastSignIn{" + "id=" + id + ", lastSignIn=" + lastSignIn + ", user=" + user + '}';
+    }
+
+    
+    
+}
--- src/java/entities/User.java
+++ src/java/entities/User.java
@@ -9,15 +9,18 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Calendar;
 import java.util.Objects;
+import static javax.persistence.CascadeType.ALL;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -86,6 +89,17 @@ public class User implements Serializable {
      * Full name for the user.
      */
     private String fullName;
+    @OneToMany(cascade = ALL, mappedBy = "user",fetch=FetchType.EAGER)
+    private LastSignIn lastSignIn;
+
+    public LastSignIn getLastSignIn() {
+        return lastSignIn;
+    }
+
+    public void setLastSignIn(LastSignIn lastSignIn) {
+        this.lastSignIn = lastSignIn;
+    }
+    
     /**
      * 
      * @return Return the id.
